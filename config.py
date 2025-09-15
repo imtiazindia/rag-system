cd09367a240b539936e72dc831dfc3b08448bf36
imtiazindia <128246603+imtiazindia@users.noreply.github.com>
2025-09-15 20:57:43 +0530

    Update config.py
    vector path is taken out

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 SUPPORTED_EXTENSIONS = ['.pdf', '.docx', '.txt']
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 100
-VECTOR_STORE_PATH = "./pdf_db"
+#VECTOR_STORE_PATH = "./pdf_db"
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
 #LLM_MODEL = "llama2"
 #LLM_MODEL = "mistral"
